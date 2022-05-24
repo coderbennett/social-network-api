a53438444af7f28f704c4f22b5e583d345aa9014
coderbennett <jbennett@csumb.edu>
2022-05-23 20:23:36 -0700

    add friend add and delete routes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -7,4 +7,7 @@ router.route('/').get(getUsers).post(createUser);
 // api -> users -> id
 router.route('/:userId').get(getUser).delete(deleteUser);
 
+// api -> users -> id -> friends -> id
+router.route('/:userId/friends/:friendId').post(addFriend).delete(removeFriend);
+
 module.exports = router;
