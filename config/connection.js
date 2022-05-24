c3240a576d64f460db38feb995ef366a22c6a35f
coderbennett <jbennett@csumb.edu>
2022-05-23 22:01:38 -0700

    add mongoose connection to connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,8 @@
-const { connect, connection } = require('mongoose');
+const mongoose = require('mongoose');
 
-const connectionString =
-process.env.MONGODB_URI || 'mongodb:localhost:27017/socialmediaDB';
-
-connect(connectionString, {
-    userNewUrlParser: true,
-    useUnifiedTopology: true,
+mongoose.connect(process.env.MONGODB_URI || 'mongodb:localhost:27017/socialmediaDB', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 });
 
-module.exports = connection;
+module.exports = mongoose.connection;
